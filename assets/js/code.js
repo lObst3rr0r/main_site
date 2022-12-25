16c1b7e59dea6be086b5440ac26b3290416a44f4
l0bst3rr0r <davcoz31@gmail.com>
2022-12-25 19:35:34 +0100

    updated

---
+++ assets/js/code.js
@@ -0,0 +1,30 @@
+var id = null;
+
+
+function left() {
+    var elem = document.getElementById("animated");
+    var pos = -30;
+    clearInterval(id);
+    id = setInterval(frame,10);
+    function frame() {
+        if (pos == 1000){
+            clearInterval(id);
+            left();
+        }
+        else {
+            pos = pos +5;
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+
+let i;
+for (i=1;i<9999;i++){
+    left();
+}
+
+
+// imagine spending christmas debugging js 
+
+
