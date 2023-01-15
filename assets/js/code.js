b1f5a0a2ba36905b5157375deb7d63d47cb6807d
l0bst3rr0r <davcoz31@gmail.com>
2023-01-15 20:56:28 +0100

    added some js

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,2 +1,10 @@
 //L//
+var machinesHtb = document.getElementById("machines-htb");
 
+function hideMachine(){
+    if(machinesHtb.style.display === "none"){
+        machinesHtb.style.display = "block";
+    } else {
+        machinesHtb.style.display = "none";
+    }
+}
